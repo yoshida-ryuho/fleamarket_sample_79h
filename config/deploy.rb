ec194280d81dcae31246e674f465455a575b6dac
yoshida-ryuho <srszu73t2mm@icloud.com>
2020-07-22 17:08:50 +0900

    deploy5

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "fleamarket_sample_79h"
-set :fleamarket_sample_79h, "git@github.com:yoshida-ryuho/fleamarket_sample_79h"
+set :repo_url, "git@github.com:yoshida-ryuho/fleamarket_sample_79h"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
