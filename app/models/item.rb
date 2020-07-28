507fecfda5250b47a92b191bc2355f02cbecae31
wakabamark0701 <wakaani0701@gmail.com>
2020-07-28 12:33:04 +0900

    pref active _hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,6 @@ class Item < ApplicationRecord
   has_one :evaluation
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :pref
 end
