470b9d980f98bb08ceebdc147ba461ae7e69d78f
kazuko-1117 <67089848+kazuko-1117@users.noreply.github.com>
2020-07-29 20:41:03 +0900

    prefモデル追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,5 @@
 class Destination < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :pref
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,17 @@ class User < ApplicationRecord
   has_many :buyer_items, foreign_key: "buyer_id", class_name: "items"
   has_one :profile, dependent: :destroy
   has_one :destination, dependent: :destroy
-  has_one :credit, dependent: :destroy       
+  has_one :credit, dependent: :destroy  
+  
+  accepts_nested_attributes_for :profiles, :destination
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
+
 end
