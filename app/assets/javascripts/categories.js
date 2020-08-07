e7f4a348277ec7142a31b3dfd1f2c70ed5765a77
kazuko-1117 <67089848+kazuko-1117@users.noreply.github.com>
2020-08-07 23:59:17 +0900

    コメントアウト削除

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,62 +1,53 @@
-$(function() {
+$(document).on('turbolinks:load', function(){
 
-  $(".HeaderInner__Block").hover(
-    function() {
-      $(this).css('background-color','#b2f6f8');
-    },
-
-    function() {
-      $(this).css('background-color','#fff');
-    }
-
-  );
-
+  $(function() {
+   
+   
+    $(".HeaderInner__List").hover(
+      function(){
+        console.log('のった')
+      $(this).find(".HeaderInner__parents").show();
+       },
+      function(){
+      $(this).find(".HeaderInner__parents").hide();
+       }
+    )
+    $(".HeaderInner__parent").hover(
+      function(){
+      $(this).children(".HeaderInner__children").show();
+       },
+      function(){
+      $(this).children(".HeaderInner__children").hide();
+       }
+    )
+   
+    $(".HeaderInner__child").hover(
+      function() {
+        $(this).children(".HeaderInner__grandchildren").show();
+      },
+      function(){
+        $(this).children(".HeaderInner__grandchildren").hide();
+      }
+    )
   
-  
-  $(".HeaderInner__Default,.HeaderInner__CategoryBox").hover(
-    function() {
-      $(".HeaderInner__CategoryBox").css("display", "block");
-    },
-    function() {
-      $(".HeaderInner__CategoryBox").css("display", "none");
-    }
-    );
-
-
-    function getChildren(child) {
-      let html = `
-          <div class="HeaderInner__Block">
-            <a class="HeaderInner__Left--first" href="/categories/1">
-              <div class="HeaderInner__Parent_name">
-                ${child.name}
-              </div>
-            </a>
-          </div>
-                  `;
-      $(".HeaderInner__CategoryBox--parents").append(html);
-    }
-
-
- 
-
-
-    $(".HeaderInner__Block").hover(function() {
-      let input = $(this).attr('value');
-      console.log(input);
-      $.ajax({
-        type: "GET",
-        url: "/categories/get_category_children",
-        data: { id: input },
-        dataType: "json"
-      })
-      .done(function() {
+        // # hoverで色を変える
+      $(".HeaderInner__parent").hover(
+        function() {
+          $(this).css('background-color','#b2f6f8');
+        },
         
-        getChildren();
-        console.log("成功です");
-      })
-      .fail(function() {
-        console.log("失敗です");
-      });
-    });
-});
-
+        function() {
+          $(this).css('background-color','#fff');
+        });
+      $(".HeaderInner__child,.HeaderInner__grandchild").hover(
+        function() {
+  
+          $(this).css('background-color','rgb(236, 235, 235)');
+        },
+        
+        function() {
+          $(this).css('background-color','#fff');
+        });
+  });
+  
+  });
