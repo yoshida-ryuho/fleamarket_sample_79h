2f0fa256395b983e4dbbdd11a115fae84321c74c
wakabamark0701 <wakaani0701@gmail.com>
2020-08-07 15:23:41 +0900

    image h w Tweak

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -11,7 +11,7 @@ $(document).on('turbolinks:load', ()=> {
   }
     // プレビュー用のimgタグを生成する関数
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `<img data-index="${index}" src="${url}" width="130px" height="70px">`;
     return html;
   }
 
